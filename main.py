952ac4e765203673084689d51bd80780105db0aa
Nacho828 <nchomaxanton@gmail.com>
2025-04-08 17:48:33 +0200

    comit :3

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
+from lanzador import lanzador
+
 if __name__ == "__main__":
-    # Imprimimos un mensaje de bienvenida
-    print("Bienvenido al programa de algoritmos 1")
-    
-    # Aquí puedes agregar la lógica principal del programa
-    # Por ejemplo, inicializar variables o llamar a funciones
-    print("El programa se está ejecutando correctamente.")
-    # Definimos las reglas de movimiento para un teclado numérico 3x3
-   
+    lanzador()
